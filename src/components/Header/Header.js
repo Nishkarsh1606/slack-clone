6f4244f95e85e0740611ddd9577c2449946c3d8b
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2022-11-25 15:03:10 +0530

    add default color for profile picture

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ import { Avatar } from '@mui/material'
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import SearchIcon from '@mui/icons-material/Search';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { purple } from '@mui/material/colors'
 
 function Header() {
     const currentUser=auth.currentUser
@@ -18,9 +19,10 @@ function Header() {
         signOut(auth)
       },500)
     }
+
   return (
     <div className='Header'>
-        <Avatar src={`${currentUser.photoURL}`} alt={'user profile picture'}/>
+        <Avatar src={`${currentUser.photoURL}`} sx={{ bgcolor: purple[900] }}/>
         <div className='search-nav'>
             <AccessTimeIcon style={{color:'white',marginRight:'50px'}}/>
             <div className='search-input'>
