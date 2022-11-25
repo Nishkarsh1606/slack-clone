75cab35b29f00b16c5a914737e6248b0c9be8ddf
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2022-11-25 15:02:34 +0530

    add delete icon to the messages

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './Posts.css'
-import { auth } from '../../firebase'
+// import { auth } from '../../firebase'
 import { Avatar } from '@mui/material'
+import { purple } from '@mui/material/colors'
+import { DeleteForever } from '@mui/icons-material'
 
-function Posts({userName,userMessage,userProfile}) {
-    const currentUser=auth.currentUser
+function Posts({userName,userMessage,userProfile,messageID}) {
     return (
-        <div className='Posts'>
-            <Avatar src={`${userProfile}`} className='user-img' />
-            <div className='user'>
-                <p className='user-info'>{userName}</p>
-                <p className='user-info'>{userMessage}</p>
+        <div className='Posts' id={messageID}>
+            <div className='main-container'>
+                <Avatar src={`${userProfile}`} sx={{ bgcolor: purple[900] }} className='user-img' />
+                <div className='user'>
+                    <p className='user-info'>{userName}</p>
+                    <p className='user-info'>{userMessage}</p>
+                </div>
+            </div>
+            <div>
+                <DeleteForever className='delete-icon'/>
             </div>
         </div>
     )
