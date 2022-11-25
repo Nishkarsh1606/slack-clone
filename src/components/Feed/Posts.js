888d9729845a9329118ca4205bdc872832d8c267
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2022-11-25 21:15:01 +0530

    add check delete only your messages

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { db } from '../../firebase'
+import { doc, deleteDoc } from 'firebase/firestore'
+import { auth } from '../../firebase'
 import './Posts.css'
 // import { auth } from '../../firebase'
 import { Avatar } from '@mui/material'
 import { purple } from '@mui/material/colors'
 import { DeleteForever } from '@mui/icons-material'
 
-function Posts({userName,userMessage,userProfile,messageID}) {
+function Posts({userName,userMessage,userProfile,messageID,userID}) {
+    const currentUser=auth.currentUser
+    const handleDelete=()=>{
+        if(currentUser.uid===userID){
+            alert('Deleted!')
+            deleteDoc(doc(db,'general',messageID))
+        }
+    }
     return (
-        <div className='Posts' id={messageID}>
+        <div className='Posts'>
             <div className='main-container'>
                 <Avatar src={`${userProfile}`} sx={{ bgcolor: purple[900] }} className='user-img' alt={userName[0]}/>
                 <div className='user'>
@@ -15,8 +25,8 @@ function Posts({userName,userMessage,userProfile,messageID}) {
                     <p className='user-info'>{userMessage}</p>
                 </div>
             </div>
-            <div>
-                <DeleteForever className='delete-icon'/>
+            <div onClick={handleDelete}>
+                <DeleteForever className={`delete-icon`}/>
             </div>
         </div>
     )
