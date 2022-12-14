5b6ac180ee0e64d2a495786e0d3abb351f2d33db
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2022-12-14 12:38:52 +0530

    misc

--- src/components/Sidebar/Features.js
+++ src/components/Sidebar/Features.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./Features.css"
-function Features({featureName,Icon}) {
+function Features({ featureName, Icon, onClickGetChannelName, channelID }) {
   return (
-    <div className='Features'>
-        <Icon/>
+    channelID ? (<div className='Features' onClick={()=>onClickGetChannelName(featureName)}>
+      <Icon />
+      <p>{featureName}</p>
+    </div>) :
+      (<div className='Features'>
+        <Icon />
         <p>{featureName}</p>
-    </div>
+      </div>)
   )
 }
 export default Features
